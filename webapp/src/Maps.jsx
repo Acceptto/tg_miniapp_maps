badffd074ea742216bf566f8b3db65b2e7218c13
Acceptto <d.shumytskyi@gmail.com>
2024-04-04 22:47:10 +0200

    refactor

--- webapp/src/Maps.jsx
+++ webapp/src/Maps.jsx
@@ -1,35 +1,42 @@
-import React, { Component } from 'react';
-import { compose, withProps } from 'recompose';
-import { withScriptjs, withGoogleMap, GoogleMap, KmlLayer } from 'react-google-maps';
+import React, { Suspense } from 'react';
+import { GoogleMap, KmlLayer, useJsApiLoader } from '@react-google-maps/api';
 
-const Map = compose(
-	withProps({
-		googleMapURL:
-			'https://maps.googleapis.com/maps/api/js?key=AIzaSyC8NppbeRAKx4bpUqCVxc87Z-DASLD_p_Q',
-		loadingElement: <div style={{ height: `100%` }} />,
-		containerElement: <div style={{ height: `100vh` }} />,
-		mapElement: <div style={{ height: `100%` }} />,
-	}),
-	withScriptjs,
-	withGoogleMap
-)(props => (
-	<GoogleMap
-		defaultZoom={2}
-		defaultCenter={{ lat: 40.23164130704827, lng: 15.088623999999982 }}
-		mapTypeId={'terrain'}
-	>
-		<KmlLayer
-			url={
-				'http://www.google.com/maps/d/u/0/kml?mid=1R8Rw7tZTGw3YHKBTNBOh-LKU5sw' +
-				'&ver=' +
-				generateRandom()
-			}
-			options={{ preserveViewport: true }}
-		/>
-	</GoogleMap>
-));
-
-function generateRandom() {
+const generateRandom = () => {
 	return Math.random() * 10000000000000000;
+};
+
+const MyMap = () => {
+	const { isLoaded } = useJsApiLoader({
+		googleMapsApiKey: 'AIzaSyC8NppbeRAKx4bpUqCVxc87Z-DASLD_p_Q',
+	});
+
+	if (!isLoaded) {
+		return <div>Loading...</div>;
+	}
+
+	return (
+		<GoogleMap
+			mapContainerStyle={{ height: '100vh' }}
+			center={{ lat: 40.23164130704827, lng: 15.088623999999982 }}
+			zoom={2}
+			mapTypeId="terrain"
+		>
+			<KmlLayer
+				url={
+					'http://www.google.com/maps/d/u/0/kml?mid=1R8Rw7tZTGw3YHKBTNBOh-LKU5sw' +
+					'&ver=' +
+					generateRandom()
+				}
+				options={{ preserveViewport: true }}
+			/>
+		</GoogleMap>
+	);
+};
+
+export default function Map() {
+	return (
+		<Suspense fallback={<div>Loading...</div>}>
+			<MyMap />
+		</Suspense>
+	);
 }
-export default Map;
