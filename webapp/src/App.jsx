56fee0400fd4e0460148266c4777f4658ef0551a
Acceptto <d.shumytskyi@gmail.com>
2024-04-04 22:58:32 +0200

    refactor

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -1,19 +1,14 @@
-
-import './App.css'
-import MainPage from './MainPage'
-import {
-	QueryClient,
-	QueryClientProvider,
-} from '@tanstack/react-query'
+import './App.css';
+import MainPage from './MainPage';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 function App() {
-
 	const queryClient = new QueryClient();
 	return (
 		<QueryClientProvider client={queryClient}>
 			<MainPage />
 		</QueryClientProvider>
-	)
+	);
 }
 
-export default App
+export default App;
