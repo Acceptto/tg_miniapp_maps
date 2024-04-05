5a227dac7924624015648d44e1daea041460070b
Acceptto <d.shumytskyi@gmail.com>
2024-04-05 10:19:02 +0200

    test

--- webapp/src/Map.jsx
+++ webapp/src/Map.jsx
@@ -1,7 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+
+const libraries = ['places'];
+const mapContainerStyle = {
+	width: '100vw',
+	height: '100vh',
+};
+const center = {
+	lat: 7.2905715, // default latitude
+	lng: 80.6337262, // default longitude
+};
 
 const Map = () => {
-	return <h1 style={{ color: 'white' }}>Hello World</h1>;
+	const { isLoaded, loadError } = useLoadScript({
+		googleMapsApiKey: 'AIzaSyC8NppbeRAKx4bpUqCVxc87Z-DASLD_p_Q',
+		libraries,
+	});
+
+	if (loadError) {
+		return <div>Error loading maps</div>;
+	}
+
+	if (!isLoaded) {
+		return <div>Loading maps</div>;
+	}
+
+	return (
+		<div>
+			<GoogleMap mapContainerStyle={mapContainerStyle} zoom={10} center={center}>
+				<Marker position={center} />
+			</GoogleMap>
+		</div>
+	);
 };
 
 export default Map;
