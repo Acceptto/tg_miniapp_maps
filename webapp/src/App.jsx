ad6cbaa6eadec8f5d3c67981f9aca62068054c55
Acceptto <d.shumytskyi@gmail.com>
2024-04-08 17:03:52 +0200

    banners with row

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -1,17 +1,12 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
-import Map from './Map';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 function App() {
 	const queryClient = new QueryClient();
 	return (
 		<QueryClientProvider client={queryClient}>
-			<Routes>
-				<Route path="/" element={<MainPage />} />
-				<Route path="/map" element={<Map />} />
-			</Routes>
+			<MainPage />
 		</QueryClientProvider>
 	);
 }
