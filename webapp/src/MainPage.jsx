81b4beac553d0181d2869c0f407492929802280a
Acceptto <d.shumytskyi@gmail.com>
2024-04-08 17:55:47 +0200

    banners with row

--- webapp/src/MainPage.jsx
+++ webapp/src/MainPage.jsx
@@ -1,16 +1,19 @@
 import { useQuery } from '@tanstack/react-query';
 import { useWebApp } from '@vkruglikov/react-telegram-web-app';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import 'react-day-picker/dist/style.css';
 import { initMiniApp } from './api';
-import Segment from './Segment.jsx';
+import { FixedLayout, SegmentedControl } from '@xelene/tgui';
+import Community from './Community';
+import Friends from './Friends';
 
 function MainPage() {
 	const { ready, initData, backgroundColor } = useWebApp();
+	const [selected, setSelected] = useState('community');
 
 	useEffect(() => {
 		ready();
-	});
+	}, [ready]);
 
 	const initResult = useQuery({
 		queryKey: ['initData'],
@@ -24,20 +27,56 @@ function MainPage() {
 
 	if (initResult.isLoading) {
 		return (
-			<div
-				style={{
-					backgroundColor,
-				}}
-			>
+			<div style={{ backgroundColor }}>
 				<div>Loading</div>
 			</div>
 		);
 	}
+
 	if (initResult.isError) {
 		return <div>Error! Try reloading the app</div>;
 	}
 
-	return <Segment token={token} />;
+	const renderContent = () => {
+		switch (selected) {
+			case 'community':
+				return <Community token={token} />;
+			case 'friends':
+				return <Friends token={token} />;
+			default:
+				return null;
+		}
+	};
+
+	const labels = [
+		{
+			label: 'Community',
+			value: 'community',
+		},
+		{
+			label: 'You&Friends',
+			value: 'friends',
+		},
+	];
+
+	return (
+		<div style={{ height: 100, width: 400 }}>
+			<FixedLayout vertical="top" style={{ padding: 16 }}>
+				<SegmentedControl style={{ maxWidth: 400, margin: '0 auto' }}>
+					{labels.map(({ value, label }) => (
+						<SegmentedControl.Item
+							key={value}
+							selected={selected === value}
+							onClick={() => setSelected(value)}
+						>
+							{label}
+						</SegmentedControl.Item>
+					))}
+				</SegmentedControl>
+			</FixedLayout>
+			<div style={{ overflowX: 'hidden' }}>{renderContent()}</div>
+		</div>
+	);
 }
 
 export default MainPage;
