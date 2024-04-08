f5518a2208b2aea797ebb893b02f563f3a2fa66b
Acceptto <d.shumytskyi@gmail.com>
2024-04-08 14:53:35 +0200

    banners with row

--- webapp/src/Community.jsx
+++ webapp/src/Community.jsx
@@ -7,27 +7,29 @@ const Community = () => {
 			id: 1,
 			header: 'Map of shipwrecks around the world',
 			subheader: 'by Dimitri',
-			before: <Image size={48} />,
+			before: <Image size={48} src="https://img.icons8.com/color/48/viking-ship.png" />,
 			children: <Button size="s">Try it out</Button>,
 		},
 		{
 			id: 2,
 			header: 'Paris with Iryna',
 			subheader: 'by Iryna',
-			before: <Image size={48} />,
+			before: <Image size={48} src="https://img.icons8.com/color/48/eiffel-tower.png" />,
 			children: <Button size="s">Try it out</Button>,
 		},
 	];
 
 	return (
-		<List style={{ background: 'var(--tgui--secondary_bg_color)' }}>
+		<List
+			style={{
+				background: 'var(--tgui--secondary_bg_color)',
+				display: 'flex',
+				flexDirection: 'column',
+				alignItems: 'flex-start', // Optional for horizontal alignment
+			}}
+		>
 			{bannersData.map(banner => (
-				<Banner
-					key={banner.id}
-					type="section"
-					style={{ marginBottom: 16, textAlign: 'left' }}
-					{...banner}
-				/>
+				<Banner key={banner.id} type="section" style={{ marginBottom: 16 }} {...banner} />
 			))}
 		</List>
 	);
